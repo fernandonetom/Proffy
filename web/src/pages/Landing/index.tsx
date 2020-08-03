b442cd74b48d99c653673c5433efcf058125fd7a
fernando39357 <fernando39357@gmail.com>
2020-08-03 13:16:04 -0300

    Link in Landing pages

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -4,6 +4,8 @@ import "./styles.css";
 import logoImg from "../../assets/images/logo.svg";
 import landingImg from "../../assets/images/landing.svg";
 
+import { Link } from "react-router-dom";
+
 import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
@@ -24,14 +26,14 @@ export default function Landing() {
 					/>
 
 					<div className="buttons-container">
-						<a href="" className="study">
+						<Link to="/study" className="study">
 							<img src={studyIcon} alt="Estudar" />
 							Estudar
-						</a>
-						<a href="" className="give-classes">
+						</Link>
+						<Link to="/give-classes" className="give-classes">
 							<img src={giveClassesIcon} alt="Estudar" />
 							Dar aulas
-						</a>
+						</Link>
 					</div>
 
 					<span className="total-connections">

8c5ff72109cfbca2f249f8736997e3acc1867f44
fernando39357 <fernando39357@gmail.com>
2020-08-05 20:51:00 -0300

    Form components and axios install

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-import "./styles.css";
-import logoImg from "../../assets/images/logo.svg";
-import landingImg from "../../assets/images/landing.svg";
+import './styles.css';
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import studyIcon from "../../assets/images/icons/study.svg";
-import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
-import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+import api from '../../services/api';
 export default function Landing() {
+	const [totalConnections, setTotalConnections] = useState(0);
+	useEffect(() => {
+		api.get('connections').then((response) => {
+			const { total } = response.data;
+			setTotalConnections(total);
+		});
+	}, []);
 	return (
 		<>
 			<div id="page-landing">
@@ -37,7 +45,7 @@ export default function Landing() {
 					</div>
 
 					<span className="total-connections">
-						total de 200 conexões já realizadas
+						total de {totalConnections} conexões já realizadas
 						<img src={purpleHeartIcon} alt="Coração roxo" />
 					</span>
 				</div>

54582d389ecb0a9ae7a8a5779a41b7e117f3c68d
fernando39357 <fernando39357@gmail.com>
2020-08-05 15:25:15 -0300

    PageHeader description

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import PageHeader from "../../components/PageHeader";
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
 
 export default function TeacherForm() {
 	return (
 		<>
 			<div id="page-teacher-form" className="container">
-				<PageHeader title="Que incrível que você quer dar aulas." />
+				<PageHeader
+					title="Que incrível que você quer dar aulas."
+					description="O primeiro passo é preencher esse formulário de inscrição"
+				/>
 			</div>
 		</>
 	);

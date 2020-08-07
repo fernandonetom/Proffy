1f3d18eb38ccd2c0578510647b232e13a420395b
fernando39357 <fernando39357@gmail.com>
2020-08-07 11:29:52 -0300

    Color icon focused

--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -40,8 +40,12 @@ function StudyTabs() {
 				component={TeacherList}
 				options={{
 					tabBarLabel: 'Proffys',
-					tabBarIcon: ({ color, size }) => (
-						<Ionicons name="ios-easel" size={size} color={color} />
+					tabBarIcon: ({ color, size, focused }) => (
+						<Ionicons
+							name="ios-easel"
+							size={size}
+							color={focused ? '#8257e5' : color}
+						/>
 					),
 				}}
 			/>
@@ -50,8 +54,12 @@ function StudyTabs() {
 				component={Favorites}
 				options={{
 					tabBarLabel: 'Favoritos',
-					tabBarIcon: ({ color, size }) => (
-						<Ionicons name="ios-heart" size={size} color={color} />
+					tabBarIcon: ({ color, size, focused }) => (
+						<Ionicons
+							name="ios-heart"
+							size={size}
+							color={focused ? '#8257e5' : color}
+						/>
 					),
 				}}
 			/>
